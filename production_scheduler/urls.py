9705608486150757fbcc969776650c3118c553b3
Felipe Gabela <gabelafelipe@gmail.com>
2020-03-22 15:24:34 -0500

    Assignment system completed!

--- production_scheduler/urls.py
+++ production_scheduler/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'production_scheduler'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('assign_order/', views.assign_order, name='assign_order'),
+    path('change_line_item_status/', views.change_line_item_status, name='change_line_item_status'),
+    path('assign_line_item_to_seamstress/', views.assign_line_item_to_seamstress, name='assign_line_item_to_seamstress'),
 ]
--- production_scheduler/views.py
+++ production_scheduler/views.py
@@ -1,33 +1,32 @@
 import urllib.parse
 import requests
 
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.conf import settings
-from django.http import Http404
-from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import cache_control, never_cache
-from django.views.generic import TemplateView, FormView
+from django.views.generic import View
 from django.urls import reverse
-from django.http import HttpResponseRedirect, HttpResponse
 from requests.auth import HTTPBasicAuth
 from django.http import JsonResponse
 from requests.exceptions import Timeout
 from requests.exceptions import ConnectionError
+
 from .models import LineItem
 from .models import Product
 from .models import Seamstress
 from .utilities import save_new_orders, retrieve_new_orders
 from .forms import AssignForm
 
-class HomeView(LoginRequiredMixin, FormView):
-    template_name='production_scheduler/home.html'
-    form_class = AssignForm
-    success_url='/'
+#from django.conf import settings
+#from django.http import Http404
+#from django.contrib.auth import logout
+#from django.views.decorators.cache import cache_control, never_cache
+#from django.http import HttpResponseRedirect, HttpResponse
+#from django.shortcuts import get_object_or_404
 
-    def get_context_data(self, **kwargs):
-        #Default initialization of context params.
+class HomeView(LoginRequiredMixin, View):
+    def get(self, request):
+        template_name='production_scheduler/home.html'
         response_status = None
         timeout, connection_error, http_error = [False] * 3
         orders = []
@@ -75,46 +74,29 @@ class HomeView(LoginRequiredMixin, FormView):
             'http_error' : http_error, 'form' : form,
             }
 
-        #if self.request.user.groups.filter(name='coordinador').exists():
+        return render(request, template_name, context)
 
-        return context
 
-    def form_invalid(self, form):
-        response = super(HomeView, self).form_invalid(form)
-        if self.request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-        else:
-            return response
-
-    def form_valid(self, form):
-        response = super(HomeView, self).form_valid(form)
-        if self.request.is_ajax():
-            assign_input = form.cleaned_data['assign']
-            line_item_id = self.request.POST['line_item_id']
-            to_update = LineItem.objects.filter(line_item_id=line_item_id).update(assigned_to=assign_input)
-            new_assignment = Seamstress.objects.filter(seamstress_id=assign_input)
-            data = {
-                'message': "Assigment Successful.",
-                'line_item_id': line_item_id,
-                'new_assigment': new_assignment[0].alias
-            }
-            return JsonResponse(data)
-        else:
-            return response
-
-#Order Assigment Ajax
 @login_required()
-def assign_order(request):
-    if request.is_ajax():
-        form = AssignForm(request.POST)
-        if form.is_valid():
-            data = {'message' : 'success'}
-            assign_input = form.cleaned_data['assign']
-            line_item_id = request.POST['line_item_id']
-            to_update = LineItem.objects.filter(line_item_id=line_item_id).update(assigned_to=assign_input)
-        else:
-            data = {'message' : form.errors}
-        return HttpResponse(json.dumps(data), content_type='aplication/json')
+def assign_line_item_to_seamstress(request):
+    if request.is_ajax() and 'assign_line_item_to_seamstress' in request.POST and request.method == "POST" :
+        assign_input = request.POST['assign']
+        line_item_id = request.POST['line_item_id']
+        to_update = LineItem.objects.filter(line_item_id=line_item_id).update(assigned_to=assign_input)
+        new_assignment = Seamstress.objects.filter(seamstress_id=assign_input)
+        data = {
+            'message': "Assigment Successful.",
+            'line_item_id': line_item_id,
+            'new_assigment': new_assignment[0].alias
+        }
+        return JsonResponse(data)
     else:
-        print('Ajax request not working!!!!')
-        raise Http404
+        return redirect('production_scheduler:home')
+
+
+@login_required()
+def change_line_item_status(request):
+    if 'change_line_item_status' in request.POST and request.method == "POST":
+        line_item_id = request.POST['line_item_id']
+        to_update = LineItem.objects.filter(line_item_id=line_item_id).update(status=1) #ASSIGNED = 1
+    return redirect('production_scheduler:home')
