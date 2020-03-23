bd0b692f2a5ae3fa4498ed5367a0678841217447
Felipe Gabela <gabelafelipe@gmail.com>
2020-03-22 21:21:47 -0500

    Update Status Almost Ready!

--- production_scheduler/forms.py
+++ production_scheduler/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from django.shortcuts import render
-from .models import Seamstress
+from .models import Seamstress, LineItem
 
 class AssignForm(forms.Form):
     assign = forms.ChoiceField( label=False,
         choices=[(seamstress.seamstress_id, seamstress.alias) for seamstress in Seamstress.objects.all()])
+
+class StatusForm(forms.Form):
+    #Choices
+    CUTTING = 2
+    ARMANDO = 3
+    TERMINADA = 4
+    STATUS_CHOICES = (
+    (CUTTING, 'Cortando'),
+    (ARMANDO, 'Armando'),
+    (TERMINADA, 'Terminada')
+    )
+    status = forms.ChoiceField( label=False, choices=STATUS_CHOICES)
