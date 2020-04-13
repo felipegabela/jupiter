45b1d2ad9df9f94a6dc1bc417146da053b10e417
Felipe Gabela <gabelafelipe@gmail.com>
2020-04-12 21:53:20 -0500

    Allowing heroku host

--- jupiter/settings.py
+++ jupiter/settings.py
@@ -13,8 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['jupiter-production-scheduler.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jupiter-production-scheduler.herokuapp.com']
 
 # Application definition
 
@@ -111,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #Heroku
 STATIC_URL = '/static/'
 
 #Format for the forms
